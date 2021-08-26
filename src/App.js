edf010825ef9f977c7ab7bb5a4bfab3eeaecf512
duhbhavesh <connect.bhavesh.kasturi@gmail.com>
2021-08-27 00:56:15 +0530

    feat: add highlights

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Highlights from './components/Highlights/Highlights';
 
 function App() {
    return (
       <div className='App'>
          <Header />
          <Hero />
+         <Highlights />
       </div>
    );
 }
---
+++ src/components/Highlights/Highlights.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Highlights.css';
+
+export default function Highlights() {
+   return (
+      <div className='container'>
+         <section id='highlights' className='highlights'>
+            <div className='highlight highlight__left--img'>
+               <div className='highlight__left'>
+                  <img
+                     className='highlight__img'
+                     src='https://res.cloudinary.com/duhbhavesh/image/upload/v1630002152/null%20brains/hightlight2_jiy3hw.png'
+                     alt='Boost Productivity'
+                  />
+               </div>
+               <div className='highlight__right'>
+                  <h3 className='highlight__title'>Boost Productivity</h3>
+                  <p className='highlight__para'>
+                     Build an atmosphere that creates productivity in your
+                     organization and your company culture.
+                  </p>
+                  <ul className='highlight__points'>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Maxmimize
+                        productivity and growth
+                     </li>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Speed past
+                        your competition
+                     </li>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Learn the
+                        top techniques
+                     </li>
+                  </ul>
+               </div>
+            </div>
+            <div className='highlight highlight__right--img'>
+               <div className='highlight__left'>
+                  <img
+                     className='highlight__img'
+                     src='https://res.cloudinary.com/duhbhavesh/image/upload/v1630002152/null%20brains/hightlight1_vldi3l.png'
+                     alt='Automated Tasks'
+                  />
+               </div>
+               <div className='highlight__right'>
+                  <h3 className='highlight__title'>Automated Tasks</h3>
+                  <p className='highlight__para'>
+                     Save time and money with our revolutionary services. We are
+                     the leaders in the industry.
+                  </p>
+                  <ul className='highlight__points'>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Automated
+                        task management workflow
+                     </li>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Detailed
+                        analytics for your data
+                     </li>
+                     <li className='highlight__point'>
+                        <span className='highlight__bullet'>✓</span> Some
+                        awesome integrations
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+      </div>
+   );
+}
